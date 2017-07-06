807b998d4fb28f802db0a6d94a2c6116b0cc8531
DannielZhang <boeing737-89l@163.com>
2017-07-06 17:27:08 +0800

    little Update

--- WebSocket/WebSocket/WebSocketHandler.cs
+++ WebSocket/WebSocket/WebSocketHandler.cs
@@ -47,52 +47,5 @@ namespace WebSocketDemo
             app.UseWebSockets();
             app.Use(WebSocketHandler.Acceptor);
         }
-
-
-        #region old
-
-
-        //static Dictionary<string, WebSocket> userDict = new Dictionary<string, WebSocket>();
-
-        //WebSocketHandler(WebSocket socket)
-        //{
-        //    this.socket = socket;
-        //    this.buffSize = 4096;
-        //}
-
-        //int buffSize;
-        //WebSocket socket;
-
-        //async Task EchoLoop()
-        //{
-        //    var buffer = new byte[buffSize];
-        //    var seg = new ArraySegment<byte>(buffer);
-
-        //    while (this.socket.State == WebSocketState.Open)
-        //    {
-        //        var incoming = await this.socket.ReceiveAsync(seg, CancellationToken.None);
-        //        var outgoing = new ArraySegment<byte>(buffer, 0, incoming.Count);
-        //        await this.socket.SendAsync(outgoing, WebSocketMessageType.Text, true, CancellationToken.None);
-        //    }
-        //}
-
-        //static async Task Acceptor(HttpContext hc, Func<Task> n)
-        //{
-        //    if (hc.WebSockets.IsWebSocketRequest)
-        //        return;
-
-        //    var socket = await hc.WebSockets.AcceptWebSocketAsync();
-
-        //    var h = new WebSocketHandler(socket);
-        //    await h.EchoLoop();
-        //}
-
-        //public static void Map(IApplicationBuilder app)
-        //{
-        //    app.UseWebSockets();
-        //    app.Use(WebSocketHandler.Acceptor);
-        //}
-        #endregion
-
     }
 }
