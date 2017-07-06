38e90391526bef4b048703da638d996e39a7c81a
DannielZhang <boeing737-89l@163.com>
2017-07-06 15:53:47 +0800

    a WebSocket demo
    First Commit
    In this version, I have build a WebSocket Server with very simple code successful.

---
+++ WebSocket/WebSocket/WebSocketHandler.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketDemo
+{
+    public class WebSocketHandler
+    {
+        public const int _bufferSize = 4096;
+        WebSocket socket;
+
+        WebSocketHandler(WebSocket socket)
+        {
+            this.socket = socket;
+        }
+
+        async Task EchoLoop()
+        {
+            var buffer = new byte[_bufferSize];
+            var seg = new ArraySegment<byte>(buffer);
+
+            while(this.socket.State == WebSocketState.Open)
+            {
+                var incoming = await this.socket.ReceiveAsync(seg, CancellationToken.None);
+                var outgoing = new ArraySegment<byte>(buffer, 0, incoming.Count);
+                await this.socket.SendAsync(outgoing, WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+        }
+
+        static async Task Acceptor(HttpContext hc, Func<Task> n)
+        {
+            if (!hc.WebSockets.IsWebSocketRequest)
+                return;
+
+            var socket = await hc.WebSockets.AcceptWebSocketAsync();
+            var h = new WebSocketHandler(socket);
+            await h.EchoLoop();
+        }
+
+        public static void Map(IApplicationBuilder app)
+        {
+            app.UseWebSockets();
+            app.Use(WebSocketHandler.Acceptor);
+        }
+
+
+        #region old
+
+
+        //static Dictionary<string, WebSocket> userDict = new Dictionary<string, WebSocket>();
+
+        //WebSocketHandler(WebSocket socket)
+        //{
+        //    this.socket = socket;
+        //    this.buffSize = 4096;
+        //}
+
+        //int buffSize;
+        //WebSocket socket;
+
+        //async Task EchoLoop()
+        //{
+        //    var buffer = new byte[buffSize];
+        //    var seg = new ArraySegment<byte>(buffer);
+
+        //    while (this.socket.State == WebSocketState.Open)
+        //    {
+        //        var incoming = await this.socket.ReceiveAsync(seg, CancellationToken.None);
+        //        var outgoing = new ArraySegment<byte>(buffer, 0, incoming.Count);
+        //        await this.socket.SendAsync(outgoing, WebSocketMessageType.Text, true, CancellationToken.None);
+        //    }
+        //}
+
+        //static async Task Acceptor(HttpContext hc, Func<Task> n)
+        //{
+        //    if (hc.WebSockets.IsWebSocketRequest)
+        //        return;
+
+        //    var socket = await hc.WebSockets.AcceptWebSocketAsync();
+
+        //    var h = new WebSocketHandler(socket);
+        //    await h.EchoLoop();
+        //}
+
+        //public static void Map(IApplicationBuilder app)
+        //{
+        //    app.UseWebSockets();
+        //    app.Use(WebSocketHandler.Acceptor);
+        //}
+        #endregion
+
+    }
+}
